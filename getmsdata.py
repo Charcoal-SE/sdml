4b24ab1bbaaaf8a806898769a3ed7a74a5695227
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-10-20 19:05:30 +0100

    finish data grabber and update data sets

--- getmsdata.py
+++ getmsdata.py
@@ -17,18 +17,25 @@ def main():
         metasmoke_api_key = sys.argv[1]
 
     for type_name, api_route in types_routes.iteritems():
+        print("Fetching data for data_type={}".format(type_name))
         data[type_name] = []
         for page in range(1, max_pages + 1):
             api_result = requests.get('https://metasmoke.erwaysoftware.com{}&key={}&page={}'.format(api_route, metasmoke_api_key, page))
-            if api_result.status() == 200:
+            status = api_result.status_code
+            print("Page {}: HTTP/1.1 {}".format(page, status))
+            if status == 200:
                 json_data = api_result.json()
                 for item in json_data['items']:
                     data[type_name].append(item)
 
                 if json_data['has_more'] == False:
                     break
+            else:
+                print("Request errored out, breaking.")
+                break
 
     for type_name, type_data in data.iteritems():
+        print("Writing data file {}.nbts.json".format(type_name))
         with open("{}.nbts.json".format(type_name), "w") as f:
             f.write(json.dumps(type_data))
 
